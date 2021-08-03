8db5bc5a34d577a4e764e67d704c768612c89fa7
Sckipper <radutdaniel96@gmail.com>
2021-08-03 22:53:16 +0300

    bulk commit DimWin

---
+++ DimWin/Forms/Brightness.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace DimWin
+{
+    partial class Brightness
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Brightness));
+            this.OverlayValue = new System.Windows.Forms.Label();
+            this.BrightnessValue = new System.Windows.Forms.Label();
+            this.BrightnessLabel = new System.Windows.Forms.Label();
+            this.ContrastLabel = new System.Windows.Forms.Label();
+            this.ContrastValue = new System.Windows.Forms.Label();
+            this.ContrastImage = new System.Windows.Forms.PictureBox();
+            this.BrightnessImage = new System.Windows.Forms.PictureBox();
+            this.OverlayImage = new System.Windows.Forms.PictureBox();
+            this.ConstrastSlider = new DimWin.NoFocusSlider();
+            this.BrightnessSlider = new DimWin.NoFocusSlider();
+            this.OverlaySlider = new DimWin.NoFocusSlider();
+            ((System.ComponentModel.ISupportInitialize)(this.ContrastImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrightnessImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OverlayImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConstrastSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrightnessSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OverlaySlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OverlayValue
+            // 
+            this.OverlayValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OverlayValue.ForeColor = System.Drawing.Color.White;
+            this.OverlayValue.Location = new System.Drawing.Point(311, 18);
+            this.OverlayValue.Name = "OverlayValue";
+            this.OverlayValue.Size = new System.Drawing.Size(42, 45);
+            this.OverlayValue.TabIndex = 1;
+            this.OverlayValue.Text = "100";
+            this.OverlayValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BrightnessValue
+            // 
+            this.BrightnessValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrightnessValue.ForeColor = System.Drawing.Color.White;
+            this.BrightnessValue.Location = new System.Drawing.Point(311, 74);
+            this.BrightnessValue.Name = "BrightnessValue";
+            this.BrightnessValue.Size = new System.Drawing.Size(42, 45);
+            this.BrightnessValue.TabIndex = 4;
+            this.BrightnessValue.Text = "100";
+            this.BrightnessValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BrightnessLabel
+            // 
+            this.BrightnessLabel.AutoSize = true;
+            this.BrightnessLabel.BackColor = System.Drawing.Color.Transparent;
+            this.BrightnessLabel.ForeColor = System.Drawing.Color.Transparent;
+            this.BrightnessLabel.Location = new System.Drawing.Point(82, 107);
+            this.BrightnessLabel.Name = "BrightnessLabel";
+            this.BrightnessLabel.Size = new System.Drawing.Size(223, 13);
+            this.BrightnessLabel.TabIndex = 6;
+            this.BrightnessLabel.Text = "0              25             50            75             100";
+            // 
+            // ContrastLabel
+            // 
+            this.ContrastLabel.AutoSize = true;
+            this.ContrastLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ContrastLabel.ForeColor = System.Drawing.Color.Transparent;
+            this.ContrastLabel.Location = new System.Drawing.Point(82, 172);
+            this.ContrastLabel.Name = "ContrastLabel";
+            this.ContrastLabel.Size = new System.Drawing.Size(223, 13);
+            this.ContrastLabel.TabIndex = 10;
+            this.ContrastLabel.Text = "0              25             50            75             100";
+            // 
+            // ContrastValue
+            // 
+            this.ContrastValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContrastValue.ForeColor = System.Drawing.Color.White;
+            this.ContrastValue.Location = new System.Drawing.Point(311, 134);
+            this.ContrastValue.Name = "ContrastValue";
+            this.ContrastValue.Size = new System.Drawing.Size(42, 45);
+            this.ContrastValue.TabIndex = 8;
+            this.ContrastValue.Text = "100";
+            this.ContrastValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ContrastImage
+            // 
+            this.ContrastImage.Image = global::DimWin.Properties.Resources.contrast_31;
+            this.ContrastImage.Location = new System.Drawing.Point(23, 140);
+            this.ContrastImage.Name = "ContrastImage";
+            this.ContrastImage.Size = new System.Drawing.Size(30, 30);
+            this.ContrastImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ContrastImage.TabIndex = 9;
+            this.ContrastImage.TabStop = false;
+            // 
+            // BrightnessImage
+            // 
+            this.BrightnessImage.Image = global::DimWin.Properties.Resources.brightness_2;
+            this.BrightnessImage.Location = new System.Drawing.Point(23, 80);
+            this.BrightnessImage.Name = "BrightnessImage";
+            this.BrightnessImage.Size = new System.Drawing.Size(30, 30);
+            this.BrightnessImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BrightnessImage.TabIndex = 5;
+            this.BrightnessImage.TabStop = false;
+            // 
+            // OverlayImage
+            // 
+            this.OverlayImage.Image = global::DimWin.Properties.Resources.contrast;
+            this.OverlayImage.Location = new System.Drawing.Point(23, 27);
+            this.OverlayImage.Name = "OverlayImage";
+            this.OverlayImage.Size = new System.Drawing.Size(35, 30);
+            this.OverlayImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.OverlayImage.TabIndex = 2;
+            this.OverlayImage.TabStop = false;
+            // 
+            // ConstrastSlider
+            // 
+            this.ConstrastSlider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ConstrastSlider.Location = new System.Drawing.Point(75, 143);
+            this.ConstrastSlider.Maximum = 100;
+            this.ConstrastSlider.Minimum = 1;
+            this.ConstrastSlider.Name = "ConstrastSlider";
+            this.ConstrastSlider.Size = new System.Drawing.Size(230, 45);
+            this.ConstrastSlider.TabIndex = 7;
+            this.ConstrastSlider.TickFrequency = 5;
+            this.ConstrastSlider.Value = 1;
+            // 
+            // BrightnessSlider
+            // 
+            this.BrightnessSlider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BrightnessSlider.Location = new System.Drawing.Point(75, 78);
+            this.BrightnessSlider.Maximum = 100;
+            this.BrightnessSlider.Name = "BrightnessSlider";
+            this.BrightnessSlider.Size = new System.Drawing.Size(230, 45);
+            this.BrightnessSlider.TabIndex = 3;
+            this.BrightnessSlider.TickFrequency = 5;
+            this.BrightnessSlider.Value = 1;
+            // 
+            // OverlaySlider
+            // 
+            this.OverlaySlider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.OverlaySlider.Location = new System.Drawing.Point(75, 27);
+            this.OverlaySlider.Maximum = 100;
+            this.OverlaySlider.Minimum = 25;
+            this.OverlaySlider.Name = "OverlaySlider";
+            this.OverlaySlider.Size = new System.Drawing.Size(230, 45);
+            this.OverlaySlider.TabIndex = 0;
+            this.OverlaySlider.TickFrequency = 5;
+            this.OverlaySlider.Value = 100;
+            // 
+            // Brightness
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(370, 210);
+            this.ControlBox = false;
+            this.Controls.Add(this.ContrastLabel);
+            this.Controls.Add(this.ContrastImage);
+            this.Controls.Add(this.ContrastValue);
+            this.Controls.Add(this.ConstrastSlider);
+            this.Controls.Add(this.BrightnessLabel);
+            this.Controls.Add(this.BrightnessImage);
+            this.Controls.Add(this.BrightnessValue);
+            this.Controls.Add(this.BrightnessSlider);
+            this.Controls.Add(this.OverlayImage);
+            this.Controls.Add(this.OverlayValue);
+            this.Controls.Add(this.OverlaySlider);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Brightness";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "dimwin";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.ContrastImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrightnessImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OverlayImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConstrastSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrightnessSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OverlaySlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NoFocusSlider OverlaySlider;
+        private System.Windows.Forms.Label OverlayValue;
+        private System.Windows.Forms.PictureBox OverlayImage;
+        private System.Windows.Forms.PictureBox BrightnessImage;
+        private System.Windows.Forms.Label BrightnessValue;
+        private NoFocusSlider BrightnessSlider;
+        private System.Windows.Forms.Label BrightnessLabel;
+        private System.Windows.Forms.Label ContrastLabel;
+        private System.Windows.Forms.PictureBox ContrastImage;
+        private System.Windows.Forms.Label ContrastValue;
+        private NoFocusSlider ConstrastSlider;
+    }
+}
+
---
+++ DimWin/Forms/Brightness.cs
@@ -0,0 +1,295 @@
+﻿/*
+    The MIT License (MIT)
+
+    Copyright (c) 2019 reblGreen Software Ltd. (https://reblgreen.com/)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+ */
+
+using System;
+using System.Drawing;
+using System.Management;
+using System.Windows.Forms;
+
+namespace DimWin
+{
+    public partial class Brightness : Form
+    {
+        NotifyIcon TrayIcon;
+        ContextMenu TrayMenu;
+        BasicWindow Overlay;
+        Timer OntopTimer;
+        bool HasChanged;
+
+        public Brightness(string[] args)
+        {
+            InitializeComponent();
+
+            SetupOntopTimer();
+
+            SetupContrastOverlay();
+
+            SetupSlidersForm();
+
+            SetupTrayIcon();
+
+        }
+
+        private void SetupOntopTimer()
+        {
+            OntopTimer = new Timer()
+            {
+                Interval = 200,
+            };
+
+            OntopTimer.Tick += (sender, e) =>
+            {
+                if (Overlay != null)
+                {
+                    Overlay.Show(false, false);
+
+                    Native.SetWindowPos(Overlay.Handle, (IntPtr)Native.HWND_TOPMOST, 0, 0, 0, 0,
+                        (int)(Native.SetWindowPosFlags.SWP_NOACTIVATE | Native.SetWindowPosFlags.SWP_NOMOVE |
+                        Native.SetWindowPosFlags.SWP_NOSIZE | Native.SetWindowPosFlags.SWP_SHOWWINDOW));
+                }
+
+               /* if (BrightnessSlider != null)
+                {
+                    BrightnessSlider.Value = Math.Max(Helpers.Brightness, 1);
+                }*/
+            };
+
+            OntopTimer.Start();
+        }
+
+        void SetupSlidersForm()
+        {
+            try
+            {
+                SetOverlayValue(Properties.Settings.Default.Brightness);
+                OverlaySliderValueChanged(null, null);
+            }
+            catch
+            {
+                OverlaySlider.Value = OverlaySlider.Maximum;
+            }
+
+            try
+            {
+                SetBrightness(GetBrightness());
+                BrightnessSliderValueChanged(null, null);
+            }
+            catch (Exception)
+            {
+                BrightnessSlider.Value = BrightnessSlider.Maximum;
+            }
+
+            OverlaySlider.LostFocus += LoseFocus;
+            BrightnessSlider.LostFocus += LoseFocus;
+            LostFocus += LoseFocus;
+
+            OverlaySlider.KeyDown += KeysDown;
+            BrightnessSlider.KeyDown += KeysDown;
+            KeyDown += KeysDown;
+
+            OverlaySlider.ValueChanged += OverlaySliderValueChanged;
+            BrightnessSlider.ValueChanged += BrightnessSliderValueChanged;
+
+            StartPosition = FormStartPosition.Manual;
+            SetLocation();
+            Visible = false;
+
+            if (Helpers.IsVistaOrHigher() && Native.DwmIsCompositionEnabled())
+            {
+                Helpers.RemoveFromAeroPeek(Handle);
+            }
+        }
+
+        void SetupContrastOverlay()
+        {
+            int top = 0;
+            int left = 0;
+            int bottom = 0;
+            int right = 0;
+
+            Overlay = new BasicWindow();
+            Helpers.SetGhost(Overlay.Handle);
+
+            foreach (Screen scr in Screen.AllScreens)
+            {
+                top = Math.Min(top, scr.Bounds.Top) - 1;
+                left = Math.Min(left, scr.Bounds.Left) - 1;
+                bottom = Math.Max(bottom, scr.Bounds.Bottom) + 2;
+                right = Math.Max(right, scr.Bounds.Right) + 2;
+            }
+
+            Overlay.SetLocation(left, top, right, bottom);
+            Overlay.TopMost = true;
+
+            Overlay.Opacity = 0;
+            Overlay.Color = Color.Black;
+            Overlay.Show(false, false);
+
+            if (Helpers.IsVistaOrHigher() && Native.DwmIsCompositionEnabled())
+            {
+                Helpers.RemoveFromAeroPeek(Overlay.Handle);
+            }
+        }
+
+        void SetupTrayIcon()
+        {
+            TrayMenu = new ContextMenu();
+            TrayMenu.MenuItems.Add("Run On Startup", RunOnStartup);
+            TrayMenu.MenuItems.Add("-");
+            TrayMenu.MenuItems.Add("Exit", OnExit);
+            TrayMenu.MenuItems[0].Checked = Startup.CheckStartup();
+
+            TrayIcon = new NotifyIcon();
+            TrayIcon.Text = "dimwin Brightness";
+            TrayIcon.Icon = Icon;
+
+            TrayIcon.Click += (object sender, EventArgs e) =>
+            {
+                SetLocation();
+                Visible = true;
+                Activate();
+                OverlaySlider.Focus();
+            };
+            
+            TrayIcon.ContextMenu = TrayMenu;
+            TrayIcon.Visible = true;
+        }
+
+        private void SetLocation()
+        {
+            var workingArea = Screen.PrimaryScreen.WorkingArea;
+            var left = workingArea.Right - Width;
+            var top = workingArea.Bottom - Height;
+            Location = new Point(left, top);
+        }
+
+        private void RunOnStartup(object sender, EventArgs e)
+        {
+            var menItm = (MenuItem)sender;
+            if (!menItm.Checked)
+            {
+                Startup.AddToStartup();
+            }
+            else
+            {
+                Startup.RemoveFromStartup();
+            }
+
+            menItm.Checked = Startup.CheckStartup();
+        }
+
+        void SetBrightness(int value)
+        {
+            //TODO
+            value = Math.Min(Math.Max(value, BrightnessSlider.Minimum), BrightnessSlider.Maximum);
+            BrightnessSlider.Value = value;
+        }
+
+        void SetOverlayValue(int value)
+        {
+            value = Math.Min(Math.Max(value, OverlaySlider.Minimum), OverlaySlider.Maximum);
+            OverlaySlider.Value = value;
+        }
+
+        void OverlaySliderValueChanged(object sender, EventArgs e)
+        {
+            var overlay = 100 - (double)OverlaySlider.Value;
+
+            if (overlay > 95)
+            {
+                overlay = 95;
+            }
+
+            Overlay.Opacity = overlay / 100;
+            OverlayValue.Text = OverlaySlider.Value.ToString();
+            Properties.Settings.Default["Brightness"] = OverlaySlider.Value;
+            HasChanged = true;
+        }
+
+        void BrightnessSliderValueChanged(object sender, EventArgs e)
+        {
+            var brightness = BrightnessSlider.Value;
+            if (brightness < 0)
+            {
+                brightness = 0;
+            }
+            byte g = 0;
+
+            if (byte.TryParse(BrightnessSlider.Value + "", out g))
+            {
+                SetBrightness(g);
+            }
+
+            Helpers.Brightness = brightness;
+            BrightnessValue.Text = BrightnessSlider.Value.ToString();
+        }
+
+        static void SetBrightness(byte targetBrightness)
+        {
+            //TODO
+        }
+        static int GetBrightness()
+        {
+            //TODO
+            return 10;
+
+        }
+
+        void KeysDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Visible = false;
+            }
+        }
+
+        void LoseFocus(object sender, EventArgs e)
+        {
+            if (!ContainsFocus)
+            {
+                Activate();
+                Visible = false;
+
+                if (HasChanged)
+                {
+                    Properties.Settings.Default.Save();
+                    HasChanged = false;
+                }
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+            base.OnLoad(e);
+        }
+ 
+        private void OnExit(object sender, EventArgs e)
+        {
+            TrayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
