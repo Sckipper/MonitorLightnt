4c1b68b16acdab55a3b86357e34bfc48c4ee92f9
Sckipper <radutdaniel96@gmail.com>
2022-04-12 11:37:59 +0300

    .NET Framework update to 4.8

--- MonitorLightn't/Properties/Settings.Designer.cs
+++ MonitorLightn't/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MonitorLightnt.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace MonitorLightnt.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
+        [global::System.Configuration.DefaultSettingValueAttribute("-40")]
         public int Brightness {
             get {
                 return ((int)(this["Brightness"]));
